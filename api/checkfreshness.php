63c6f82157d30aad01aebd907f5520e1e07d8197
Nightfly <nightfly@email.ua>
2023-03-17 19:19:29 +0200

    apachezen module backported

--- api/checkfreshness.php
+++ api/checkfreshness.php
@@ -7,12 +7,12 @@ require_once ('libs/api.ubrouting.php');
 if (ubRouting::optionCliCheck('run', false)) {
 
     $diffDumpFlag = ubRouting::optionCliCheck('dumpdiff', false);
-    $errorCount=0;
+    $errorCount = 0;
 
     $ubillingLibsPath = __DIR__ . '/../../ubilling/api/libs/';
     $yalfLibsPath = __DIR__ . '/libs/';
 
-    $ignoreList = array('api.compat.php', 'api.ubconfig.php', 'api.mysql.php'); //that libs may be different
+    $ignoreList = array('api.compat.php', 'api.ubconfig.php', 'api.mysql.php', 'api.eventview.php', 'api.processmon.php', 'api.taskbar.php', 'api.apachezen.php'); //that libs may be different
     $ignoreList = array_flip($ignoreList);
 
     $allYalfLibs = rcms_scandir($yalfLibsPath, '*.php');
@@ -26,7 +26,7 @@ if (ubRouting::optionCliCheck('run', false)) {
                     if (!isset($ignoreList[$eachYalfLib])) {
                         $diffResult = shell_exec('diff --ignore-all-space ' . $yalfLibsPath . $eachYalfLib . ' ' . $ubillingLibsPath . $eachYalfLib);
                         if (!empty($diffResult)) {
-                        	$errorCount++;
+                            $errorCount++;
                             print('FAILED: ' . $eachYalfLib . PHP_EOL);
                             if ($diffDumpFlag) {
                                 print('=========================' . PHP_EOL);
@@ -42,10 +42,10 @@ if (ubRouting::optionCliCheck('run', false)) {
 
             //summary here
             print('=========================' . PHP_EOL);
-            if ($errorCount>0) {
-            	print('Found '.$errorCount.' issues with libs freshness'.PHP_EOL);
+            if ($errorCount > 0) {
+                print('Found ' . $errorCount . ' issues with libs freshness' . PHP_EOL);
             } else {
-            	print('Everything is Ok'.PHP_EOL);
+                print('Everything is Ok' . PHP_EOL);
             }
         } else {
             print('Error: no Ubilling libs at specified path not found: ' . $ubillingLibsPath . PHP_EOL);
