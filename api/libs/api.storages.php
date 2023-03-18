7f635c5760d7d45985e916aefe047e72fb582668
Nightfly <nightfly@email.ua>
2023-03-18 16:01:14 +0200

    basic storages management looks like it works

--- api/libs/api.storages.php
+++ api/libs/api.storages.php
@@ -19,9 +19,34 @@ class Storages {
      */
     protected $allStorages = array();
 
+    /**
+     * Contains system messages helper instance
+     *
+     * @var object
+     */
+    protected $messages = '';
+
+    /**
+     * Some predefined stuff here
+     */
+    const PROUTE_PATH = 'newstoragepath';
+    const PROUTE_NAME = 'newstoragename';
+    const ROUTE_DEL = 'deletestorageid';
+    const URL_ME = '?module=storages';
+
     public function __construct() {
+        $this->initMessages();
         $this->initStoragesDb();
         $this->loadStorages();
+    }
+
+    /**
+     * Inits system messages helper
+     * 
+     * @return void
+     */
+    protected function initMessages() {
+        $this->messages = new UbillingMessageHelper();
     }
 
     /**
@@ -75,6 +100,113 @@ class Storages {
         } else {
             $result = __('Storage path or name is empty');
         }
+        return($result);
+    }
+
+    /**
+     * Deletes some storage from database
+     * 
+     * @param int $storageId
+     * 
+     * @return void/string on error
+     */
+    public function delete($storageId) {
+        $result = '';
+        $storageId = ubRouting::filters($storageId, 'int');
+        if (isset($this->allStorages[$storageId])) {
+            if (!$this->isProtected($storageId)) {
+                $this->storagesDb->where('id', '=', $storageId);
+                $this->storagesDb->delete();
+                log_register('STORAGE DELETE [' . $storageId . ']');
+            } else {
+                $result = __('You can not delete storage which is in usage');
+            }
+        } else {
+            $result = __('No such storage') . ' [' . $storageId . ']';
+        }
+
+        return($result);
+    }
+
+    /**
+     * Renders storage creation form
+     * 
+     * @return string
+     */
+    public function renderCreationForm() {
+        $result = '';
+        $inputs = wf_TextInput(self::PROUTE_PATH, __('Path'), '', false, 20);
+        $inputs .= wf_TextInput(self::PROUTE_NAME, __('Name'), '', false, 20);
+        $inputs .= wf_Submit(__('Create'));
+        $result .= wf_Form('', 'POST', $inputs, 'glamour');
+        return($result);
+    }
+
+    /**
+     * Checks is storage path exists, valid and writtable?
+     * 
+     * @param string $path
+     * 
+     * @return bool
+     */
+    protected function checkPath($path) {
+        $result = false;
+        if (file_exists($path)) {
+            if (is_dir($path)) {
+                if (is_writable($path)) {
+                    $result = true;
+                }
+            }
+        }
+        return($result);
+    }
+
+    /**
+     * Renders available storages list
+     * 
+     * @return string
+     */
+    public function renderList() {
+        $result = '';
+        if (!empty($this->allStorages)) {
+            $cells = wf_TableCell(__('ID'));
+            $cells .= wf_TableCell(__('Path'));
+            $cells .= wf_TableCell(__('Name'));
+            $cells .= wf_TableCell(__('State'));
+            $cells .= wf_TableCell(__('Free'));
+            $cells .= wf_TableCell(__('Actions'));
+            $rows = wf_TableRow($cells, 'row1');
+            foreach ($this->allStorages as $io => $each) {
+                $cells = wf_TableCell($each['id']);
+                $cells .= wf_TableCell($each['path']);
+                $cells .= wf_TableCell($each['name']);
+                $storageState = ($this->checkPath($each['path'])) ? true : false;
+                $stateIcon = web_bool_led($storageState);
+                $storageFree = ($storageState) ? stg_convert_size(disk_free_space($each['path'])) : '-';
+                $cells .= wf_TableCell($stateIcon);
+                $cells .= wf_TableCell($storageFree);
+                $actControls = wf_JSAlert(self::URL_ME . '&' . self::ROUTE_DEL . '=' . $each['id'], web_delete_icon(), $this->messages->getDeleteAlert());
+                $cells .= wf_TableCell($actControls);
+                $rows .= wf_TableRow($cells, 'row5');
+            }
+
+            $result .= wf_TableBody($rows, '100%', 0, 'sortable');
+        } else {
+            $result .= $this->messages->getStyledMessage(__('Nothing to show'), 'warning');
+        }
+        return($result);
+    }
+
+    /**
+     * Checks is some storage used by some cameras?
+     * 
+     * @param int $storageId
+     * 
+     * @return bool
+     */
+    protected function isProtected($storageId) {
+        $result = false;
+        //TODO: do here few checks to prevent deletion of storage which is used now
         return($result);
     }
 
--- modules/general/storages/index.php
+++ modules/general/storages/index.php
@@ -1,8 +1,30 @@
 <?php
 
 if (cfr('ROOT')) {
-    $storages=new Storages();
-    
+    $storages = new Storages();
+
+    //storage creation
+    if (ubRouting::checkPost(array($storages::PROUTE_PATH, $storages::PROUTE_NAME))) {
+        $creationResult = $storages->create(ubRouting::post($storages::PROUTE_PATH), ubRouting::post($storages::PROUTE_NAME));
+        if ($creationResult) {
+            show_error($creationResult);
+        } else {
+            ubRouting::nav($storages::URL_ME);
+        }
+    }
+
+    //storage deletion
+    if (ubRouting::checkGet($storages::ROUTE_DEL)) {
+        $deletionResult = $storages->delete(ubRouting::get($storages::ROUTE_DEL));
+        if ($deletionResult) {
+            show_error($deletionResult);
+        } else {
+            ubRouting::nav($storages::URL_ME);
+        }
+    }
+
+    show_window(__('Create new storage'), $storages->renderCreationForm());
+    show_window(__('Available storages'), $storages->renderList());
 } else {
     show_error(__('Access denied'));
 }
