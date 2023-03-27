320599a77d4f5b50ccd794fed0f0692314e7e0d3
Nightfly <nightfly@email.ua>
2023-03-28 01:07:22 +0300

    wrong branch fixed

--- dist/services/wrcurrent.sh
+++ dist/services/wrcurrent.sh
@@ -6,13 +6,12 @@ cd ${BASEPATH}/tmp/
 rm -fr ../wr_current.tgz > /dev/null
 rm -fr ../RELEASE > /dev/null
 rm -fr master.zip
-/usr/local/bin/curl -k -s https://codeload.github.com/nightflyza/WolfRecorder/zip/master > master.zip
-/usr/local/bin/unzip master.zip -d ../exported
-cd ../exported/WolfRecorder-master/
+/usr/local/bin/curl -k -s https://codeload.github.com/nightflyza/WolfRecorder/zip/main > main.zip
+/usr/local/bin/unzip main.zip -d ../exported
+cd ../exported/WolfRecorder-main/
 rm -fr nbproject
 tar cf - ./* | gzip > ../../wr_current.tgz
 cp -R ./RELEASE ${BASEPATH}/RELEASE
 cp -R ./dist/wrinstaller.sh ${BASEPATH}/wrinstaller_current.sh
 cd ${BASEPATH}
 rm -fr exported ./tmp/*
-
