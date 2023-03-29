e2ea3de88b1749551019f4ccda2795170158503c
Nightfly <nightfly@email.ua>
2023-03-29 18:11:48 +0300

    cosmetics again

--- dist/wrinstaller.sh
+++ dist/wrinstaller.sh
@@ -144,7 +144,7 @@ fi
 $DIALOG --infobox "Unpacking binary packages." 4 60
 tar zxvf ${ARCH}${DL_PACKAGES_EXT} 2>> ${INSTALLER_LOG}
 cd ${ARCH}
-$DIALOG --infobox "Software installation is in progress. This takes a while." 4 60
+$DIALOG --infobox "Software installation is in progress. This takes a while." 4 70
 ls -1 | xargs -n 1 pkg add >> ${INSTALLER_LOG}
 $DIALOG --infobox "Binary packages installation has been completed." 4 60
 
