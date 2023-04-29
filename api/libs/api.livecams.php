7f5b939c8b7ea504c56a5feb62c4c5e69236c182
Nightfly <nightfly@email.ua>
2023-04-29 15:03:50 +0300

    livestreams still in progress

--- api/libs/api.livecams.php
+++ api/libs/api.livecams.php
@@ -139,7 +139,7 @@ class LiveCams {
         $this->ffmpgPath = $this->binPaths['FFMPG_PATH'];
         $this->cdPath = $this->binPaths['CD'];
         $this->liveOptsPrefix = '-stimeout 5000000 -loglevel error -rtsp_transport tcp -f rtsp -i';
-        $this->liveOptsSuffix = '-strict -2 -vcodec copy -hls_wrap 99';
+        $this->liveOptsSuffix = '-strict -2 -vcodec copy -hls_wrap 10';
         $this->streamsPath = Storages::PATH_HOWL . self::STREAMS_SUBDIR;
     }
 
@@ -401,7 +401,6 @@ class LiveCams {
         $result = '';
         $streamUrl = $this->getStreamUrl($channelId);
         if ($streamUrl) {
-            deb($streamUrl);
             $playerId = 'liveplayer_' . $channelId;
             $player = new Player($this->playerWidth, true);
             $result .= $player->renderLivePlayer($streamUrl, $playerId);
