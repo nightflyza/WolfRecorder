5c9eb50cd1c624aa335e1e30ca77fa18db7bcc77
Nightfly <nightfly@email.ua>
2023-11-23 11:53:41 +0200

    i18n: romanian locale added

---
+++ languages/romanian/main.php
@@ -0,0 +1,90 @@
+<?php
+
+////////////////////////////////////////////////////////////////////////////////
+//   Copyright (C) ReloadCMS Development Team                                 //
+//   http://reloadcms.sf.net                                                  //
+//                                                                            //
+//   This program is distributed in the hope that it will be useful,          //
+//   but WITHOUT ANY WARRANTY, without even the implied warranty of           //
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     //
+//                                                                            //
+//   This product released under GNU General Public License v2                //
+////////////////////////////////////////////////////////////////////////////////
+
+/* * *******************************************************************************
+ * Timezones                                                                      *
+ * ******************************************************************************* */
+$lang['tz']['-12'] = 'GMT - 12 Hours';
+$lang['tz']['-11'] = 'GMT - 11 Hours';
+$lang['tz']['-10'] = 'GMT - 10 Hours';
+$lang['tz']['-9'] = 'GMT - 9 Hours';
+$lang['tz']['-8'] = 'GMT - 8 Hours';
+$lang['tz']['-7'] = 'GMT - 7 Hours';
+$lang['tz']['-6'] = 'GMT - 6 Hours';
+$lang['tz']['-5'] = 'GMT - 5 Hours';
+$lang['tz']['-4'] = 'GMT - 4 Hours';
+$lang['tz']['-3.5'] = 'GMT - 3.5 Hours';
+$lang['tz']['-3'] = 'GMT - 3 Hours';
+$lang['tz']['-2'] = 'GMT - 2 Hours';
+$lang['tz']['-1'] = 'GMT - 1 Hours';
+$lang['tz']['0'] = 'GMT';
+$lang['tz']['1'] = 'GMT + 1 Hour';
+$lang['tz']['2'] = 'GMT + 2 Hours';
+$lang['tz']['3'] = 'GMT + 3 Hours';
+$lang['tz']['3.5'] = 'GMT + 3.5 Hours';
+$lang['tz']['4'] = 'GMT + 4 Hours';
+$lang['tz']['4.5'] = 'GMT + 4.5 Hours';
+$lang['tz']['5'] = 'GMT + 5 Hours';
+$lang['tz']['5.5'] = 'GMT + 5.5 Hours';
+$lang['tz']['6'] = 'GMT + 6 Hours';
+$lang['tz']['6.5'] = 'GMT + 6.5 Hours';
+$lang['tz']['7'] = 'GMT + 7 Hours';
+$lang['tz']['8'] = 'GMT + 8 Hours';
+$lang['tz']['9'] = 'GMT + 9 Hours';
+$lang['tz']['9.5'] = 'GMT + 9.5 Hours';
+$lang['tz']['10'] = 'GMT + 10 Hours';
+$lang['tz']['11'] = 'GMT + 11 Hours';
+$lang['tz']['12'] = 'GMT + 12 Hours';
+$lang['tz']['13'] = 'GMT + 13 Hours';
+
+/* * *******************************************************************************
+ * Date localization                                                              *
+ * ******************************************************************************* */
+$lang['datetime']['Sunday'] = 'Duminica';
+$lang['datetime']['Monday'] = 'Luni';
+$lang['datetime']['Tuesday'] = 'Marti';
+$lang['datetime']['Wednesday'] = 'Miercuri';
+$lang['datetime']['Thursday'] = 'Joi';
+$lang['datetime']['Friday'] = 'Vineri';
+$lang['datetime']['Saturday'] = 'Subota';
+$lang['datetime']['Sun'] = 'Du';
+$lang['datetime']['Mon'] = 'Lu';
+$lang['datetime']['Tue'] = 'Ma';
+$lang['datetime']['Wed'] = 'Mie';
+$lang['datetime']['Thu'] = 'Jo';
+$lang['datetime']['Fri'] = 'Vi';
+$lang['datetime']['Sat'] = 'Si';
+$lang['datetime']['January'] = 'Ianuarie';
+$lang['datetime']['February'] = 'Februarie';
+$lang['datetime']['March'] = 'Martier';
+$lang['datetime']['April'] = 'Aprilie';
+$lang['datetime']['May'] = 'Mai';
+$lang['datetime']['June'] = 'Iunie';
+$lang['datetime']['July'] = 'Iulie';
+$lang['datetime']['August'] = 'August';
+$lang['datetime']['September'] = 'Septembrie';
+$lang['datetime']['October'] = 'Octombrie';
+$lang['datetime']['November'] = 'Noiembrie';
+$lang['datetime']['December'] = 'Decembrie';
+$lang['datetime']['Jan'] = 'Ian';
+$lang['datetime']['Feb'] = 'Feb';
+$lang['datetime']['Mar'] = 'Mar';
+$lang['datetime']['Apr'] = 'Apr';
+$lang['datetime']['Ma'] = 'Mai';
+$lang['datetime']['Jun'] = 'Iun';
+$lang['datetime']['Jul'] = 'Iul';
+$lang['datetime']['Aug'] = 'Aug';
+$lang['datetime']['Sep'] = 'Sep';
+$lang['datetime']['Oct'] = 'Oct';
+$lang['datetime']['Nov'] = 'Noi';
+$lang['datetime']['Dec'] = 'Oct';
