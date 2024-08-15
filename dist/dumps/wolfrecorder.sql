e904ef802b2edb1313ed774665654cf5b9b7e08d
Nightfly <nightfly@email.ua>
2024-08-15 13:15:20 +0300

    scheme update

--- dist/dumps/wolfrecorder.sql
+++ dist/dumps/wolfrecorder.sql
@@ -93,6 +93,7 @@ CREATE TABLE IF NOT EXISTS `camopts` (
   `cameraid` INT(11) NOT NULL,
   `rtspport` INT(11) DEFAULT NULL,
   `keepsubalive` tinyint(2) NOT NULL DEFAULT 0,
+  `order` INT(11) DEFAULT NULL,
   PRIMARY KEY  (`id`),
   KEY `cameraid` (`cameraid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
