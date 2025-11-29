024b421bf1fed837bd40fcb589bc33d2c71c7028
Nightfly <nightfly@email.ua>
2025-11-29 15:05:17 +0200

    14.3M ready for testing

--- dist/wrinstaller.sh
+++ dist/wrinstaller.sh
@@ -47,6 +47,7 @@ $DIALOG --menu "Type of WolfRecorder installation" 10 75 8 \
 clear
 
 $DIALOG --menu "Choose FreeBSD version and architecture" 16 50 8 \
+                   143_6M "FreeBSD 14.3 amd64"\
                    143_6L "FreeBSD 14.3 amd64"\
                    142_6L "FreeBSD 14.2 amd64"\
                    135_6L "FreeBSD 13.5 amd64"\
